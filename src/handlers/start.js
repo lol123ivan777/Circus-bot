3c031e63a768e34af5df42cb5ecb4b6ec1b49d36
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 02:28:38 +0300

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,66 +1,17 @@
 // src/handlers/start.js
 const { inlineMenuKeyboard } = require('../keyboards/inlineMenu');
 
-exports.handleStart = async (bot, input, msgId = null) => {
-  // input may be callback_query or message object
-  const query = input && input.data ? input : null;
-  const msg = query ? query.message : input;
-  const chatId = msg?.chat?.id || input?.chat?.id || input?.from?.id;
+exports.handleStart = async (bot, input) => {
+  const msg = input.message || input;
+  const chatId = msg.chat.id;
 
-  if (!chatId) {
-    console.error('START: chatId not found');
-    return;
-  
-  const bannerUrl = 'https://i.imgur.com/4AiXzf8.jpeg';
+  const bannerUrl = 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Nikulin_Circus.jpg';
 
   const caption =
     '🎪 *Добро пожаловать в цирк Никулина!* \n\n' +
-    'Здесь вы можете узнать расписание представлений, ' +
-    'ознакомиться с артистами и посмотреть новости.\n\n' +
-    'Выберите раздел в меню ниже.';
+    'Добро пожаловать в официальный Telegram-бот цирка Никулина. ' +
+    'Выберите раздел ниже, чтобы продолжить.';
 
-  // If msgId present - try to edit existing (smart logic handled in editSmart for content types)
-  if (msgId) {
-    try {
-      // if original message was photo -> edit caption; if text -> edit text
-      const originalMsg = query ? query.message : null;
-      if (originalMsg && originalMsg.photo) {
-        return bot.editMessageCaption(caption, {
-          chat_id: chatId,
-          message_id: msgId,
-          parse_mode: 'Markdown',
-          reply_markup: inlineMenuKeyboard.reply_markup
-        }).catch(err => {
-          // ignore "message is not modified"
-          if (err?.response?.body?.description && err.response.body.description.includes('message is not modified')) {
-            return;
-          }
-          throw err;
-        });
-      } else {
-        return bot.editMessageText(caption, {
-          chat_id: chatId,
-          message_id: msgId,
-          parse_mode: 'Markdown',
-          reply_markup: inlineMenuKeyboard.reply_markup
-        }).catch(err => {
-          if (err?.response?.body?.description && err.response.body.description.includes('message is not modified')) {
-            return;
-          }
-          throw err;
-        });
-      }
-    } catch (e) {
-      // fallback send new
-      return bot.sendPhoto(chatId, bannerUrl, {
-        caption,
-        parse_mode: 'Markdown',
-        reply_markup: inlineMenuKeyboard.reply_markup
-      });
-    }
-  }
-
-  // normal start: send banner photo with inline menu
   return bot.sendPhoto(chatId, bannerUrl, {
     caption,
     parse_mode: 'Markdown',
