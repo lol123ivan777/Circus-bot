400da2f208b9711b3d70e96942d8a5d88c7988b8
lol123ivan777 <idiidmmme@gmail.com>
2025-12-07 15:35:00 +0300

    Update mainMenu.js

---
+++ src/keyboards/mainMenu.js
@@ -0,0 +1,13 @@
+const mainMenuKeyboard = {
+  reply_markup: {
+    keyboard: [
+      ['🎪 О цирке', '📰 Новости'],
+      ['🌟 Артисты', '🎭 Программы'],
+      ['🎫 Билеты', '📍 Контакты'],
+      ['⬅️ Назад в меню']
+    ],
+    resize_keyboard: true
+  }
+};
+
+module.exports = { mainMenuKeyboard };
