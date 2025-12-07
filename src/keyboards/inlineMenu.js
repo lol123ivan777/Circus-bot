4fe00034d2591f3996bc2e7e89383eaa710f22b4
lol123ivan777 <idiidmmme@gmail.com>
2025-12-07 17:29:54 +0300

    Create inlineMenu.js

---
+++ src/keyboards/inlineMenu.js
@@ -0,0 +1,21 @@
+// src/keyboards/inlineMenu.js
+
+const inlineMenuKeyboard = {
+  reply_markup: {
+    inline_keyboard: [
+      [
+        { text: '🎪 О цирке', callback_data: 'about' },
+        { text: '📰 Новости', callback_data: 'news' }
+      ],
+      [
+        { text: '🌟 Артисты', callback_data: 'artists' },
+        { text: '🎭 Расписание', callback_data: 'schedule' }
+      ],
+      [
+        { text: '⬅️ Назад в меню', callback_data: 'back_to_menu' }
+      ]
+    ]
+  }
+};
+
+module.exports = { inlineMenuKeyboard };
