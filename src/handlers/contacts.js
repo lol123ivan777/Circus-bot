d7be4621eb61f59458d8e43fa386da581deff1b7
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 02:55:28 +0300

    Update contacts.js

--- src/handlers/contacts.js
+++ src/handlers/contacts.js
@@ -2,33 +2,31 @@
 const { editSmart } = require('../utils/editSmart');
 
 exports.handleContacts = async (bot, input) => {
-  const text =
-`📞 *Контакты цирка Никулина*\n
-*Адрес:*
+  const text = 
+`📞 Контакты цирка Никулина
+
+Адрес:
 Москва, Цветной бульвар, 13
 
-*Телефон кассы:* +7 495 628 83 49
-*Администрация:* +7 495 780 31 35
+Телефон кассы:
++7 495 628 83 49
 
-*E-mail:*
+Администрация:
++7 495 780 31 35
+
+E-mail:
 info@circusnikulin.ru
 pr@circusnikulin.ru
 
-*Соцсети:*
+Соцсети:
 VK: https://vk.com/circusnikulin
 Telegram: https://t.me/nikulin_circus
 `;
 
   const inline_keyboard = [
-    [
-      { text: '🌐 Открыть сайт', url: 'https://circusnikulin.ru/' }
-    ],
-    [
-      { text: '📍 Открыть в Яндекс.Картах', url: 'https://yandex.ru/maps/-/CCUuFCoxcB' }
-    ],
-    [
-      { text: '⬅️ Назад в меню', callback_data: 'back_to_menu' }
-    ]
+    [{ text: '🌐 Открыть сайт', url: 'https://circusnikulin.ru/' }],
+    [{ text: '📍 Яндекс.Карты', url: 'https://yandex.ru/maps/-/CCUuFCoxcB' }],
+    [{ text: '⬅️ Назад', callback_data: 'back_to_menu' }]
   ];
 
   return editSmart(bot, input, text, { inline_keyboard });
