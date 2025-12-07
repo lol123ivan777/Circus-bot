a072c28cbe54bb2b378e1ea99d391c320522657a
lol123ivan777 <idiidmmme@gmail.com>
2025-12-07 17:06:55 +0300

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,17 +1,22 @@
+// src/handlers/start.js
+
 exports.handleStart = async (bot, msg) => {
-const chatId = msg.chat.id;
+  const chatId = msg.chat.id;
 
-const bannerUrl = 'https://i.imgur.com/4AiXzf8.jpeg';
+  const bannerUrl = 'https://i.imgur.com/4AiXzf8.jpeg';
 
-const caption =
-'🎪 Добро пожаловать в цирк Никулина! \n\n' +
-'Здесь вы можете узнать расписание представлений, ознакомиться с артистами, ' +
-'просмотреть новости и получить полезную информацию.\n\n' +
-'Выберите нужный раздел в меню ниже.';
+  const caption =
+    '🎪 *Добро пожаловать в цирк Никулина!* \n\n' +
+    'Здесь вы можете узнать расписание представлений, ' +
+    'ознакомиться с артистами, просмотреть новости ' +
+    'и получить полезную информацию.\n\n' +
+    'Выберите нужный раздел в меню ниже.';
 
-await bot.sendPhoto(chatId, bannerUrl, {
-caption,
-parse_mode: 'Markdown',
-reply_markup: require('../keyboards/mainMenu').mainMenuKeyboard.reply_markup
-});
+  const { mainMenuKeyboard } = require('../keyboards/mainMenu');
+
+  await bot.sendPhoto(chatId, bannerUrl, {
+    caption,
+    parse_mode: 'Markdown',
+    reply_markup: mainMenuKeyboard.reply_markup
+  });
 };
