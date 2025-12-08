3ba1938d730672a56eba9ab1fcacf2d1e0b2e387
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 09:09:38 +0300

    Update navigation.js

--- src/utils/navigation.js
+++ src/utils/navigation.js
@@ -1,4 +1,5 @@
 // src/utils/navigation.js
+
 const editSmart = require('./editSmart');
 
 async function go(bot, input, text, extra = {}) {
@@ -6,7 +7,7 @@ async function go(bot, input, text, extra = {}) {
 }
 
 async function goPhoto(bot, input, url, caption, extra = {}) {
-  // при желании можно сделать отдельную логику для фото
+  // если захочешь потом делать фото через editMedia — тут допилим
   return editSmart(bot, input, caption, extra);
 }
 
