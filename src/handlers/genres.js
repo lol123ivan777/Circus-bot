c9fdaeeb4014dbb58f3a8af45049f49a69717bd3
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 04:01:39 +0300

    Update genres.js

--- src/handlers/genres.js
+++ src/handlers/genres.js
@@ -1,14 +1,13 @@
 exports.handleGenres = async (bot, input) => {
   return go(bot, input,
-    "🐅 *Жанры циркового искусства*\n\n" +
-    "Выберите интересующий жанр.",
-    {
-      inline_keyboard: [
-        [{ text: "🤹 Жонглирование", callback_data: "genre:juggling" }],
-        [{ text: "🎭 Клоунада", callback_data: "genre:clown" }],
-        [{ text: "🦅 Воздушные гимнасты", callback_data: "genre:aerial" }],
-        [{ text: "⬅️ Назад", callback_data: "back_to_menu" }]
-      ]
-    }
-  );
+"🎭 *Жанры циркового искусства*\n\n" +
+"Выберите интересующий жанр.",
+  {
+    inline_keyboard: [
+      [{ text: "🎪 Жонглирование", callback_data: "genre:juggling" }],
+      [{ text: "🤡 Клоунада", callback_data: "genre:clown" }],
+      [{ text: "🤸 Воздушные гимнасты", callback_data: "genre:aerial" }],
+      [{ text: "⬅️ Назад", callback_data: "back_to_menu" }]
+    ]
+  });
 };
