7a2447f0af53c6279c01b06b22fd546ad59c87a8
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 04:12:47 +0300

    Update news.js

--- src/handlers/news.js
+++ src/handlers/news.js
@@ -1,9 +1,11 @@
+const editSmart = require('../utils/editSmart');
+
 exports.handleNews = async (bot, input) => {
   const text =
 "📰 *Новости цирка*\n\n" +
 "Последние события.";
 
-  return go(bot, input, text, {
+  return editSmart(bot, input, text, {
     inline_keyboard: [
       [{ text: "Загрузить ещё", callback_data: "news_more" }],
       [{ text: "⬅️ Назад", callback_data: "back_to_menu" }]
