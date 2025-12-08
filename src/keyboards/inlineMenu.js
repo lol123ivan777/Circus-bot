ac968fee1a71ff9259cf0419e770dec1380afa8e
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 03:27:46 +0300

    Update inlineMenu.js

--- src/keyboards/inlineMenu.js
+++ src/keyboards/inlineMenu.js
@@ -1,4 +1,5 @@
 // src/keyboards/inlineMenu.js
+
 const inlineMenuKeyboard = {
   reply_markup: {
     inline_keyboard: [
@@ -11,10 +12,8 @@ const inlineMenuKeyboard = {
         { text: '📅 Расписание', callback_data: 'schedule' }
       ],
       [
+        { text: '🐅 Жанры', callback_data: 'genres' },
         { text: '🎟 Билеты', callback_data: 'tickets' }
-      ],
-      [
-        { text: '🐅 Жанры цирка', callback_data: 'genres' }
       ],
       [
         { text: '☎️ Контакты', callback_data: 'contacts' }
