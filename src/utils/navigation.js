45a6956e3c42e63428d8f3f360c81737770f54f6
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 04:46:39 +0300

    Update navigation.js

--- src/utils/navigation.js
+++ src/utils/navigation.js
@@ -1,13 +1,13 @@
-// Унифицированная навигация, гарантия отсутствия спама
+// src/utils/navigation.js
+const editSmart = require('./editSmart');
 
-const { editSmart } = require('./editSmart');
-
-async function go(bot, input, text, keyboard) {
-  return editSmart(bot, input, text, keyboard);
+async function go(bot, input, text, extra = {}) {
+  return editSmart(bot, input, text, extra);
 }
 
-async function goPhoto(bot, input, url, caption, keyboard) {
-  return editSmart(bot, input, caption, keyboard);
+async function goPhoto(bot, input, url, caption, extra = {}) {
+  // при желании можно сделать отдельную логику для фото
+  return editSmart(bot, input, caption, extra);
 }
 
 module.exports = { go, goPhoto };
