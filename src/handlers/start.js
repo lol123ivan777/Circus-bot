6443e714cc95e7848d0a3586e4e130b3f310f040
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 03:31:11 +0300

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,20 +1,14 @@
-// src/handlers/start.js
 const { inlineMenuKeyboard } = require('../keyboards/inlineMenu');
 
 exports.handleStart = async (bot, input) => {
   const msg = input.message || input;
   const chatId = msg.chat.id;
 
-  const bannerUrl = 'https://i.imgur.com/4AiXzf8.jpeg';
-
-  const caption =
-    '🎪 *Добро пожаловать в цирк Никулина!* \n\n' +
-    'Добро пожаловать в официальный Telegram-бот цирка Никулина. ' +
-    'Выберите раздел ниже, чтобы продолжить.';
-
-  return bot.sendPhoto(chatId, bannerUrl, {
-    caption,
-    parse_mode: 'Markdown',
+  return bot.sendPhoto(chatId, "https://i.imgur.com/4AiXzf8.jpeg", {
+    caption:
+    "🎪 *Цирк Никулина*\n\n" +
+    "Добро пожаловать! Выберите раздел ниже.",
+    parse_mode: "Markdown",
     reply_markup: inlineMenuKeyboard.reply_markup
   });
 };
