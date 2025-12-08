3f8ddf2d7a9e509c5cebc9728a2af4f89995291c
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 09:11:56 +0300

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,33 +1,20 @@
 // src/handlers/start.js
-const { inlineMenuKeyboard } = require('../keyboards/inlineMenu');
-const { editSmart } = require('../utils/editSmart');
 
-const START_PHOTO_URL = 'https://i.imgur.com/4AiXzf8.jpeg';
+const { inlineMenuKeyboard } = require('../keyboards/inlineMenu');
 
 exports.handleStart = async (bot, input) => {
-  const isCallback = !!input.data;
   const msg = input.message || input;
   const chatId = msg.chat.id;
 
-  // если это /start (обычное сообщение) — показываем фото один раз
-  if (!isCallback) {
-    await bot.sendPhoto(chatId, START_PHOTO_URL, {
+  return bot.sendPhoto(
+    chatId,
+    'https://i.imgur.com/4AiXzf8.jpeg',
+    {
       caption:
         '🎪 *Цирк Никулина*\n\n' +
-        'Добро пожаловать в легендарный цирк на Цветном бульваре.',
-      parse_mode: 'Markdown'
-    });
-  }
-
-  const text =
-    '🎪 *Цирк Никулина*\n\n' +
-    'Выберите раздел ниже.';
-
-  // для callback редактируем сообщение с кнопками
-  // для /start создаём новое сообщение с меню
-  const targetInput = isCallback ? input : { chat: { id: chatId } };
-
-  return editSmart(bot, targetInput, text, {
-    inline_keyboard: inlineMenuKeyboard.reply_markup.inline_keyboard
-  });
+        'Добро пожаловать! Выберите раздел ниже.',
+      parse_mode: 'Markdown',
+      reply_markup: inlineMenuKeyboard.reply_markup
+    }
+  );
 };
