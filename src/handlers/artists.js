ea913b1a4b5599739259d46d3fe6830b4dbfa3c2
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 04:48:06 +0300

    Update artists.js

--- src/handlers/artists.js
+++ src/handlers/artists.js
@@ -1,14 +1,16 @@
+// src/handlers/artists.js
 const editSmart = require('../utils/editSmart');
 
 exports.handleArtists = async (bot, input) => {
   const text =
-"✨ *Артисты цирка*\n\n" +
-"Артисты Цирка Никулина на Цветном бульваре .";
+    '✨ *Артисты цирка*\n\n' +
+    'Артисты Цирка Никулина на Цветном бульваре — ведущие мастера циркового искусства.\n' +
+    'В репертуаре — акробаты, воздушные гимнасты, клоуны, жонглёры и многие другие.';
 
   return editSmart(bot, input, text, {
     inline_keyboard: [
-      [{ text: "Загрузить ещё", callback_data: "artists_more" }],
-      [{ text: "⬅️ Назад", callback_data: "back_to_menu" }]
+      [{ text: 'Загрузить ещё', callback_data: 'artists_more' }],
+      [{ text: '⬅️ Назад в меню', callback_data: 'back_to_menu' }]
     ]
   });
 };
