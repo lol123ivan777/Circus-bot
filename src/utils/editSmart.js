e677c0c920a2f19f3d718868d160b604835c5213
lol123ivan777 <idiidmmme@gmail.com>
2025-12-08 08:21:52 +0300

    Update editSmart.js

--- src/utils/editSmart.js
+++ src/utils/editSmart.js
@@ -1,31 +1,20 @@
-// src/utils/editSmart.js
+async function editSmart(bot, input, text, keyboard) {
+  const chatId = input.message?.chat?.id || input.chat?.id;
+  const messageId = input.message?.message_id;
 
-module.exports = async function editSmart(bot, input, text, extra = {}) {
-  const opts = {
-    parse_mode: 'Markdown',
-    ...extra
-  };
-
-  if (extra.inline_keyboard && !opts.reply_markup) {
-    opts.reply_markup = { inline_keyboard: extra.inline_keyboard };
-  }
-
-  // callback_query: редактируем существующее сообщение
-  if (input?.message?.message_id) {
-    const chatId = input.message.chat.id;
-    const msgId = input.message.message_id;
-
+  if (messageId) {
     return bot.editMessageText(text, {
       chat_id: chatId,
-      message_id: msgId,
-      ...opts
+      message_id: messageId,
+      parse_mode: 'Markdown',
+      reply_markup: keyboard
     });
   }
 
-  // обычное сообщение: отправляем новое
-  if (input?.chat?.id) {
-    return bot.sendMessage(input.chat.id, text, opts);
-  }
+  return bot.sendMessage(chatId, text, {
+    parse_mode: 'Markdown',
+    reply_markup: keyboard
+  });
+}
 
-  console.error('editSmart: неизвестный тип input', input);
-};
+module.exports = { editSmart };
